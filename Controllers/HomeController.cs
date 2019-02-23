1665cae23719308b2623f3a54b2418135848ecb6
prescottbreeden <prescott.breeden@gmail.com>
2019-02-23 15:07:06 -0800

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using view_model_fun.Models;
+
+namespace view_model_fun.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            User bob = new User();
+            return View(bob);
+        }
+
+        [HttpGet("firstname")]
+        public IActionResult FirstName()
+        {
+            User bob = new User();
+            return View("firstname", bob.FirstName);
+        }
+
+        [HttpGet("lastname")]
+        public IActionResult LastName()
+        {
+            User bob = new User();
+            return View("lastname", bob.LastName);
+        }
+
+        [HttpGet("fullname")]
+        public IActionResult FullName()
+        {
+            User bob = new User();
+            string name = $"{bob.FirstName} {bob.LastName}";
+            return View("fullname", name);
+        }
+
+        [HttpGet("numbers")]
+        public IActionResult Numbers()
+        {
+            User bob = new User();
+            return View("numbers", bob.FavoriteNumbers);
+        }
+
+        [HttpGet("friends")]
+        public IActionResult BestFriends()
+        {
+            User bob = new User();
+            return View("friends", bob.BestFriends);
+        }
+
+        [HttpGet("all")]
+        public IActionResult All()
+        {
+            User bob = new User();
+            return View("allinfo", bob);
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace view_model_fun.Models
+{
+    public class User
+    {
+      public string FirstName { get; set; } = "Bob";
+      public string LastName { get; set; } = "Ross";
+      public List<int> FavoriteNumbers { get; set; } = new List<int> 
+      {
+          1, 2, 3, 10, 43, 5
+      };
+      public List<string> BestFriends { get; set; } = new List<string>
+      {
+          "Sally", "Billy", "Joey", "Moose"
+      };
+    }
+}
